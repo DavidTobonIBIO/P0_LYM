ed9c2bc10df6c240e239f7340906cf1cb3428f4b
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2024-02-06 14:36:48 -0500

    test

--- InstructionParser.py
+++ InstructionParser.py
@@ -1,5 +1,3 @@
 class InstructionParser:
     def __init__(self):
         pass
-
-    # TODO: distinguish between blocks, variables, action commands, control structures, functions and conditions, and send them to the right parser
